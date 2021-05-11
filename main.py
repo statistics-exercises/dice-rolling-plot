f6945f14bbae052e8d0e7ff37d487d764c4c0789
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 16:11:37 +0100

    Added exercise

---
+++ main.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def uniform_discrete(a, b) :
+  # Your code for generating a uniform discrete random variable goes here
+  
+# Your code for generating the scatter plot of dice rolls variables goes here
